91d85a111528fb8a85f2709d9e6ba43d3c692df1
Muste Abari <m.abari@hotmail.co.uk>
2020-12-15 10:22:07 +0000

    finished but complete button doesnt work

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@35.246.60.242/flaskdb"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
+app.config["SECRET_KEY"] = getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)
 
